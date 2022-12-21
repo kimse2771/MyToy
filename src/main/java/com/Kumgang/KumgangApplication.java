699472fa35db91c13ffaf69fb01b8d26d9e11cc0
kws39 <kws39@DESKTOP-DUHD2PK>
2022-12-22 01:42:49 +0900

    init commit

---
+++ src/main/java/com/Kumgang/KumgangApplication.java
@@ -0,0 +1,13 @@
+package com.Kumgang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumgangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumgangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kumgang/KumgangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kumgang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumgangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
